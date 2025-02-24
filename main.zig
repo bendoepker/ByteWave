3ef074ccda25e7aa57b952d946913c769c54053d
Ben Doepker <bendoepk@gmail.com>
2025-02-24 10:10:15 -0500

    updated gitignore, moved files

--- main.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const pa = @cImport({
-    @cInclude("portaudio.h");
-});
-
-pub fn main() !void {}
--- structs/audio_structs.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-/// 32 bit float 2 channel point
-const f32_2c_point = struct {
-    left: f32,
-    right: f32,
-};
