dffa3c5a94b56bdcfe7420bcbde5510183ee4e63
Ben Doepker <bendoepk@gmail.com>
2025-06-04 23:17:45 -0400

    removed unused macros

--- include/bw-config.h
+++ include/bw-config.h
@@ -3,12 +3,6 @@
 #include <bw-types.h>
 #include <stdint.h>
 
-#define BW_HOST_API_TAG_WASAPI "WAPI"
-#define BW_HOST_API_TAG_ASIO "ASIO"
-#define BW_HOST_API_TAG_JACK "JACK"
-#define BW_HOST_API_TAG_ALSA "ALSA"
-#define BW_HOST_API_TAG_CORE_AUDIO "CORE"
-
 BWError BWConfig_Write(char* file_name, BWConfigData* data);
 BWError BWConfig_Read(char* file_name, BWConfigData* data);
 
